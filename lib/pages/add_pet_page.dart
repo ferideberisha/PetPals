3565a16999d174872b1a3259ef89c5cbf498b5a2
ferideberisha <93921511+ferideberisha@users.noreply.github.com>
2024-05-23 00:54:31 +0200

    Added dropdown list to AddPet page
    Removed component dropdown list

--- lib/pages/add_pet_page.dart
+++ lib/pages/add_pet_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:petpals/components/dropdown_list.dart';
 import 'package:petpals/components/my_button.dart'; // Import MyButton
 import 'package:petpals/components/circle_avatar.dart'; // Import CircleAvatarWidget
 import 'package:petpals/components/my_textfield.dart'; // Import your custom text field
@@ -25,7 +24,6 @@ class _AddPetPageState extends State<AddPetPage> {
   bool _isHouseTrained = false;
   bool _isFriendlyWithCats = false;
 
-  String? _selectedWalksPerDay = 'Not specified'; // Set default value
 
   Future<void> _pickImage(ImageSource source) async {
     final pickedImage = await ImagePicker().pickImage(source: source);
@@ -89,7 +87,7 @@ class _AddPetPageState extends State<AddPetPage> {
             ),
             SizedBox(height: 20),
             Divider(),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -100,7 +98,7 @@ class _AddPetPageState extends State<AddPetPage> {
                     fontSize: 16,
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 15),
                 MyTextField(
                   controller: _nameController,
                   hintText: 'Enter your pet\'s name',
@@ -140,8 +138,7 @@ class _AddPetPageState extends State<AddPetPage> {
                           });
                         },
                         text: 'Male',
-                        color:
-                            _isMaleSelected ? Color(0xFFCAADEE) : Colors.white,
+                        color: _isMaleSelected ? Color(0xFFCAADEE) : Colors.white,
                         textColor: Colors.black,
                         borderColor: Color(0xFFCAADEE),
                         borderWidth: 2,
@@ -157,9 +154,7 @@ class _AddPetPageState extends State<AddPetPage> {
                           });
                         },
                         text: 'Female',
-                        color: _isFemaleSelected
-                            ? Color(0xFFCAADEE)
-                            : Colors.white,
+                        color: _isFemaleSelected ? Color(0xFFCAADEE) : Colors.white,
                         textColor: Colors.black,
                         borderColor: Color(0xFFCAADEE),
                         borderWidth: 2,
@@ -182,9 +177,7 @@ class _AddPetPageState extends State<AddPetPage> {
                       });
                     },
                     text: '1-5',
-                    color: _selectedSizeRange == '1-5'
-                        ? Color(0xFFCAADEE)
-                        : Colors.white,
+                    color: _selectedSizeRange == '1-5' ? Color(0xFFCAADEE) : Colors.white,
                     textColor: Colors.black,
                     borderColor: Color(0xFFCAADEE),
                     borderWidth: 2,
@@ -199,9 +192,7 @@ class _AddPetPageState extends State<AddPetPage> {
                       });
                     },
                     text: '5-10',
-                    color: _selectedSizeRange == '5-10'
-                        ? Color(0xFFCAADEE)
-                        : Colors.white,
+                    color: _selectedSizeRange == '5-10' ? Color(0xFFCAADEE) : Colors.white,
                     textColor: Colors.black,
                     borderColor: Color(0xFFCAADEE),
                     borderWidth: 2,
@@ -216,9 +207,7 @@ class _AddPetPageState extends State<AddPetPage> {
                       });
                     },
                     text: '10-15',
-                    color: _selectedSizeRange == '10-15'
-                        ? Color(0xFFCAADEE)
-                        : Colors.white,
+                    color: _selectedSizeRange == '10-15' ? Color(0xFFCAADEE) : Colors.white,
                     textColor: Colors.black,
                     borderColor: Color(0xFFCAADEE),
                     borderWidth: 2,
@@ -233,9 +222,7 @@ class _AddPetPageState extends State<AddPetPage> {
                       });
                     },
                     text: '15-20',
-                    color: _selectedSizeRange == '15-20'
-                        ? Color(0xFFCAADEE)
-                        : Colors.white,
+                    color: _selectedSizeRange == '15-20' ? Color(0xFFCAADEE) : Colors.white,
                     textColor: Colors.black,
                     borderColor: Color(0xFFCAADEE),
                     borderWidth: 2,
@@ -252,9 +239,7 @@ class _AddPetPageState extends State<AddPetPage> {
                   fontWeight: FontWeight.normal,
                   fontSize: 16,
                 )),
-            SizedBox(height: 10),
-            // Inside the Column widget, under the 'About the pet' text
-            SizedBox(height: 10),
+            SizedBox(height: 15),
             Container(
               height: 100,
               decoration: BoxDecoration(
@@ -276,80 +261,92 @@ class _AddPetPageState extends State<AddPetPage> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 15),
             SwitchListTile(
               title: Text(
                 'Microchipped',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isMicrochipped,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isMicrochipped = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6), // Set active track color
             ),
             SwitchListTile(
               title: Text(
-                'Friendly with Children',
+                'Friendly with children',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isFriendlyWithChildren,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isFriendlyWithChildren = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6),
             ),
             SwitchListTile(
               title: Text(
-                'Spayed or Neutered',
+                'Spayed or neutered',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isSpayedOrNeutered,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isSpayedOrNeutered = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6),
             ),
             SwitchListTile(
               title: Text(
-                'Friendly with Dogs',
+                'Friendly with dogs',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isFriendlyWithDogs,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isFriendlyWithDogs = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6),
             ),
             SwitchListTile(
               title: Text(
-                'House Trained',
+                'House trained',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isHouseTrained,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isHouseTrained = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6),
             ),
             SwitchListTile(
               title: Text(
-                'Friendly with Cats',
+                'Friendly with cats',
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
               ),
               value: _isFriendlyWithCats,
-              onChanged: (value) {
+              onChanged: (bool value) {
                 setState(() {
                   _isFriendlyWithCats = value;
                 });
               },
+              activeColor: Colors.white,
+              activeTrackColor: Color(0xFF967BB6),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 5),
             Divider(),
             SizedBox(height: 10),
             Text('Care info',
@@ -358,40 +355,82 @@ class _AddPetPageState extends State<AddPetPage> {
                   fontWeight: FontWeight.normal,
                   fontSize: 16,
                 )),
-            SizedBox(height: 10),
-            // Inside the Column widget, under the 'Vet info' text
-            SizedBox(height: 10),
-            CustomDropdownList(
-              items: [
-                'Number of walks per day',
-                'Not specified',
-                '1',
-                '2',
-                '3',
-                '4',
-                '5 or more',
-              ],
-              value: _selectedWalksPerDay,
-              onChanged: (String? value) {
+            SizedBox(height: 20),
+            // Inside the _AddPetPageState class, in the build method
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: 'Number of walks per day',
+                labelStyle: TextStyle(color: Colors.grey[500]),
+                 hintText: 'Select Option', // Display hint text inside dropdown
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Color.fromRGBO(226, 225, 225, 1)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Color(0xFF967BB6)),
+                ),
+              ),
+              value: 'Not specified', // Default value
+              onChanged: (String? newValue) {
+                // Handle dropdown value change
                 setState(() {
-                  _selectedWalksPerDay =
-                      value ?? 'Not specified'; // Handle nullability
+                  // Update the state with the new value
                 });
               },
-              itemStyles: [
-                TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                ), // 'Number of walks per day'
-                if (_selectedWalksPerDay !=
-                    'Not specified') // Check if 'Not specified' is selected
-                  TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
-                  ), // All other items
-              ],
+              items: <String>[
+                'Not specified',
+                'One walk per day',
+                'Two walks per day',
+                'Three walks per day',
+                'Four walks per day',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                      value,
+                      style: TextStyle(fontWeight: FontWeight.normal), // Set font weight to normal
+                    ),
+                );
+              }).toList(),
             ),
-
+              SizedBox(height: 20),
+            // Inside the _AddPetPageState class, in the build method
+            DropdownButtonFormField<String>(
+              decoration: InputDecoration(
+                labelText: 'Energy level',
+                labelStyle: TextStyle(color: Colors.grey[500]),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Color.fromRGBO(226, 225, 225, 1)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Color(0xFF967BB6)),
+                ),
+              ),
+              value: 'Not specified', // Default value
+              onChanged: (String? newValue) {
+                // Handle dropdown value change
+                setState(() {
+                  // Update the state with the new value
+                });
+              },
+              items: <String>[
+                'Not specified',
+                'High',
+                'Medium',
+                'Low',
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(
+                      value,
+                      style: TextStyle(fontWeight: FontWeight.normal), // Set font weight to normal
+                    ),
+                );
+              }).toList(),
+            ),
             SizedBox(height: 20),
             Divider(),
             SizedBox(height: 10),
@@ -401,8 +440,6 @@ class _AddPetPageState extends State<AddPetPage> {
                   fontWeight: FontWeight.normal,
                   fontSize: 16,
                 )),
-            SizedBox(height: 10),
-            // Inside the Column widget, under the 'About the pet' text
             SizedBox(height: 10),
             Container(
               height: 100,
@@ -427,16 +464,17 @@ class _AddPetPageState extends State<AddPetPage> {
               ),
             ),
             SizedBox(height: 20),
-            MyButton(
-              onTap: () {
-                // Handle Add Pet logic here
-                print('Add Pet button tapped!');
-              },
-              text: 'Add Pet',
-              color: Color(0xFF967BB6), // Change the color as needed
-              textColor: Colors.white,
-              borderColor: Color(0xFF967BB6),
-              borderWidth: 0,
+            Center(
+              child: MyButton(
+                onTap: () {
+                  // Handle form submission
+                },
+                text: 'Submit',
+                color: Color(0xFF967BB6),
+                textColor: Colors.white,
+                borderColor: Color(0xFF967BB6),
+                borderWidth: 2,
+              ),
             ),
           ],
         ),
